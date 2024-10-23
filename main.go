1bee4fc80636b0b8a4073b6341af9a82891ba362
Eugene Makarikhin <contact@aboutslack.info>
2024-10-23 13:35:15 +0200

    Added viper and config file

--- main.go
+++ main.go
@@ -8,24 +8,24 @@ import (
 
 	"simplebank/api"
 	db "simplebank/db/sqlc"
-)
-
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"simplebank/util"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatal("connot connect to db:", err)
+		log.Fatal("can not load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("can not connect to db:", err)
 	}
 
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("can not start")
 	}
