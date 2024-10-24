66f506463ab0a2340e89866518dc2ab4fba608f6
Evgenii Makarikhin <wkdjgf534@users.noreply.github.com>
2024-10-24 16:45:40 +0200

    Account REST API (#2)
    * Added account endpoints and functions
    
    * Updated github CIDI file again
    
    * Added viper and config file
    
    * Updated test.yml
    
    * Removed const from main_test.go
    
    * Added gomock, generated the first mock
    
    * Added first unit test for account api

--- Makefile
+++ Makefile
@@ -20,5 +20,11 @@ sqlc:
 test:
 	go test -v -cover -race ./...
 
-.PHONY: postgres createdb dropdb migrateup migratedown sqlc
+server:
+	go run main.go
+
+mock:
+	mockgen -package mockdb -destination db/mock/store.go simplebank/db/sqlc Store
+
+.PHONY: postgres createdb dropdb migrateup migratedown sqlc server mock
 
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,22 +6,27 @@ import (
 	"fmt"
 )
 
-// Store provides all functions to execute db queriess and transactions
-type Store struct {
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+// SQLStore provides all functions to execute SQL queries and transactions
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
 // NewStore creates a new Store
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // execTx executes a function within a database transaction
-func (store *Store) execTX(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTX(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil) // nil - use default DB isolation level
 	if err != nil {
 		return err
@@ -59,7 +64,7 @@ type TransferTxResult struct {
 
 // TransferTx performs a money transfer from one account to the other.
 // It creates a transfer record, add account entries, and update accounts' balance within a single database transaction
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTX(ctx, func(q *Queries) error {
