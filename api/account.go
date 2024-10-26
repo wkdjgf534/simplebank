0bed8cec7c66c1fe34ec3a3c83299066bce6e380
Evgenii Makarikhin <wkdjgf534@users.noreply.github.com>
2024-10-26 23:08:00 +0200

    Transfer REST API (#3)
    * Added transfer api
    
    * Added unit test for transfer api

--- api/account.go
+++ api/account.go
@@ -10,7 +10,7 @@ import (
 
 type createAccountRequest struct {
 	Owner    string `json:"owner" binding:"required"`
-	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+	Currency string `json:"currency" binding:"required,currency"` // currency - it is a custom validator
 }
 
 type getAccountRequest struct {
--- util/random.go
+++ util/random.go
@@ -42,7 +42,7 @@ func RandomMoney() int64 {
 
 // RandomCurrency generates a random currency code
 func RandomCurrency() string {
-	currencies := []string{"CAD", "EUR", "USD"}
+	currencies := []string{EUR, USD, CAD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
